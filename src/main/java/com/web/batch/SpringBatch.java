9a11ef31d6db51c379ab1a3b6b8f225fcff835fa
pawan_kushwah <pawan.k@contus.in>
2018-06-27 10:40:43 +0530

    CSV file upload by batch processing

---
+++ src/main/java/com/web/batch/SpringBatch.java
@@ -0,0 +1,12 @@
+package com.web.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatch {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatch.class, args);
+	}
+}
